727eeb60fc27ce59e0523afb836136c7a9060012
architjhaug23-droid <archit.jha.ug23@nsut.ac.in>
2026-06-27 18:18:38 +0530

    Delete categoryRoutes.js

--- categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllCategories, getCategory } = require('../controllers/categoryController');
-
-router.get('/', getAllCategories);
-router.get('/:slug', getCategory);
-
-module.exports = router;
